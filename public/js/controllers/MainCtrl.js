d836a6a4836b8a726a29dc33eeeb59f7188d0af1
shravansabavat <shravan.sabavat@gmail.com>
2016-02-28 20:03:47 -0500

    initial code

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,8 +1,3 @@
 angular.module('MainCtrl', []).controller('MainController', function($scope) {
-	$scope.isActive = false;
-
-	$scope.toggleActive = function() {
-		$scope.isActive = !$scope.isActive;
-	};
 
 });
